6c39246e2219b9fab1c246bc5652b86975a50ead
Stefan Niclas Heun <niclas.heun@tum.de>
2025-02-01 21:22:57 +0100

    adding support to store own course progression in course store

--- prompt-shared-state/src/zustand/useCourseStore.tsx
+++ prompt-shared-state/src/zustand/useCourseStore.tsx
@@ -4,15 +4,19 @@ import { Course } from '../interfaces/course/course'
 interface CourseStoreState {
   selectedCourse?: Course
   courses: Course[]
+  ownCourseIDs: string[]
 }
 
 interface CourseStoreAction {
   setSelectedCourse: (selectedCourse?: Course) => void
   setCourses: (courses: Course[]) => void
+  setOwnCourseIDs: (ownCourseIDs: string[]) => void
+  isStudentOfCourse: (courseID: string) => boolean
 }
 
 export const useCourseStore = create<CourseStoreState & CourseStoreAction>((set) => ({
   courses: [],
+  ownCourseIDs: [],
   setSelectedCourse: (selectedCourse?: Course) => {
     if (selectedCourse) {
       localStorage.setItem('selected-course', selectedCourse.id)
@@ -23,4 +27,8 @@ export const useCourseStore = create<CourseStoreState & CourseStoreAction>((set)
     set({ selectedCourse })
   },
   setCourses: (courses: Course[]) => set({ courses }),
+  setOwnCourseIDs: (ownCourseIDs: string[]) => set({ ownCourseIDs }),
+  isStudentOfCourse: (courseID: string): boolean => {
+    return useCourseStore.getState().ownCourseIDs.includes(courseID)
+  },
 }))
